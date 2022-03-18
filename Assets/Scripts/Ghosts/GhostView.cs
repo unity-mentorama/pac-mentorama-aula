eb17e45a63e772a302488a3ce6647fbaf15d5ee8
Lex Melotti <>
2022-03-18 09:53:11 -0300

    Ghost view

---
+++ Assets/Scripts/Ghosts/GhostView.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public enum GhostType
+{
+	Blinky,
+	Pinky,
+	Inky,
+	Clyde
+}
+
+public class GhostView : MonoBehaviour
+{
+	public CharacterMotor CharacterMotor;
+
+	public GhostAI GhostAI;
+
+	public Animator Animator;
+
+	public GhostType GhostType;
+
+	public void Start()
+	{
+		Animator.SetInteger("GhostType", (int)GhostType);
+		CharacterMotor.OnDirectionChanged += CharacterMotor_OnDirectionChanged;
+	}
+
+	private void CharacterMotor_OnDirectionChanged(Direction direction)
+	{
+		Animator.SetInteger("Direction", (int)direction - 1);
+	}
+}
